3bc1f8b6ebdab191b2419c1cd583daf6a0ef8258
FelesFurva <kitten.solar@gmail.com>
2022-05-06 20:55:58 +0300

    - added function to calculate pawn nextposition - added function to calculate pawn global position (should work even if the starting position isn't 0,0) - created a lable array instead of a coordinates array - created a movement function for pawn which takes in lable name and returns lable coordinats

--- LudoWinForm/LudoWinForm/Form1.cs
+++ LudoWinForm/LudoWinForm/Form1.cs
@@ -5,41 +5,37 @@ namespace LudoWinForm
         Image[] diceImages;
         int dice;
         Random random = new Random();
-        int[,] boardcoordinates;
+        Label[] boardtiles;
         int stepsmade1 = 0;
         int stepsmade2 = 0;
         int boxnumber1 = 0;
         int boxnumber2 = 0;
+
+        int playerIndex = 0;
+
+        const int startShift = 0;
+        const int sharedCellsCount = 6;
 
         PictureBox[] pawns;
 
         public Form1()
         {
             InitializeComponent();
+            boardtiles = new Label[7] { lbl_sqr0, lbl_sqr1, lbl_sqr2, lbl_sqr3, lbl_sqr4, lbl_sqr5, lbl_sqr6 };
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            diceImages = new Image[7];
-            diceImages[0] = Properties.Resources.Dice_blank;
-            diceImages[1] = Properties.Resources.Dice_1;
-            diceImages[2] = Properties.Resources.Dice_2;
-            diceImages[3] = Properties.Resources.Dice_3;
-            diceImages[4] = Properties.Resources.Dice_4;
-            diceImages[5] = Properties.Resources.Dice_5;
-            diceImages[6] = Properties.Resources.Dice_6;
-
-            Label[] sharedCells = new Label[40];
-
-
-            boardcoordinates = new int[7,2] { 
-                                        { lbl_sqr0.Location.X, lbl_sqr0.Location.Y },
-                                        { lbl_sqr1.Location.X, lbl_sqr1.Location.Y },
-                                        { lbl_sqr2.Location.X, lbl_sqr2.Location.Y },
-                                        { lbl_sqr3.Location.X, lbl_sqr3.Location.Y },
-                                        { lbl_sqr4.Location.X, lbl_sqr4.Location.Y },
-                                        { lbl_sqr5.Location.X, lbl_sqr5.Location.Y },
-                                        { lbl_sqr6.Location.X, lbl_sqr6.Location.Y },
+            diceImages = new Image[7]
+            {
+                Properties.Resources.Dice_blank,
+                Properties.Resources.Dice_1,
+                Properties.Resources.Dice_2,
+                Properties.Resources.Dice_3,
+                Properties.Resources.Dice_4,
+                Properties.Resources.Dice_5,
+                Properties.Resources.Dice_6
             };
         }
 
@@ -50,7 +46,7 @@ namespace LudoWinForm
 
         private void RollDice()
         {
-            dice = random.Next(2, 7);
+            dice = random.Next(1, 7);
             lbl_dice.Image = diceImages[dice];
             
         }
@@ -67,31 +63,30 @@ namespace LudoWinForm
 
         private void pawn1_Click(object sender, EventArgs e)
         {
-            if (pawn1.Left == nest1.Location.X && pawn1.Top == nest1.Location.Y)
-            {
+            if (pawn1.Location == nest1.Location)
+            {   
                 if (dice == 6)
                 {
-                    pawn1.Location = lbl_sqr0.Location;
-                    pawn1.Move(sharedCells[boxnumber1])
-                    //pawn1.Left = boardcoordinates[0, 0];
-                    //pawn1.Top = boardcoordinates[0, 1];
+                    pawn1.MoveTo(lbl_sqr0);
                 }
 
             }
             else if (boxnumber1 >= 0 && dice > 0)
             {
-                boxnumber1 += dice;
+                var individualT = nextPosition(stepsmade1, dice);
 
-                pawn1.Left = boardcoordinates[boxnumber1, 0];
-                pawn1.Top = boardcoordinates[boxnumber1, 1];
+                var individualPosition = individualT;
 
-                stepsmade1 += dice;
-                dice = 0;
+                var globalT = globalPosition(0, individualPosition);
+
+                boxnumber1 = globalT;
+
+                pawn1.MoveTo(boardtiles[boxnumber1]);
+                stepsmade1 = individualT;
 
                 if (stepsmade1 == 6)
                 {
-                    pawn1.Left = nest1.Location.X;
-                    pawn1.Top = nest1.Location.Y;
+                    pawn1.MoveTo(nest1);
                     pawn1.Visible = false;
                 }
             }
@@ -99,45 +94,44 @@ namespace LudoWinForm
 
         private void pawn2_Click(object sender, EventArgs e)
         {
-            if (pawn2.Left == nest2.Location.X && pawn2.Top == nest2.Location.Y)
+            if (pawn2.Location == nest2.Location)
             {
                 if (dice == 6)
                 {
-                    pawn2.Left = boardcoordinates[0, 0];
-                    pawn2.Top = boardcoordinates[0, 1];
+                    pawn2.MoveTo(boardtiles[0]);
                 }
 
             }
             else if (boxnumber2 >= 0 && dice > 0)
             {
 
-                boxnumber2 += dice;
-                int checking = boxnumber2 + dice;
+                var individualT = nextPosition(stepsmade2, dice);
 
-                if (checking > 6)
-                {
-                    int stepsback = (boxnumber2 + dice) - 6;
-                    boxnumber2 = boxnumber2 - stepsback;
+                var individualPosition = individualT;
 
-                    pawn2.Left = boardcoordinates[boxnumber2, 0];
-                    pawn2.Top = boardcoordinates[boxnumber2, 1];
-                }
+                var globalT = globalPosition(0, individualPosition);
 
-                else
-                {
-                    pawn2.Left = boardcoordinates[boxnumber2, 0];
-                    pawn2.Top = boardcoordinates[boxnumber2, 1];
+                boxnumber2 = globalT;
 
-                    stepsmade2 += dice;
-                }
-                dice = 0;
+                pawn2.MoveTo(boardtiles[boxnumber2]);
+                stepsmade2 = individualT;
+
                 if (stepsmade2 == 6)
                 {
-                    pawn2.Left = nest2.Location.X;
-                    pawn2.Top = nest2.Location.Y;
+                    pawn2.MoveTo(nest2);
                     pawn2.Visible = false;
                 }
             }
+        }
+
+        private static int nextPosition(int currentPosition, int diceRollValue)
+        {
+            return sharedCellsCount - Math.Abs(sharedCellsCount - (currentPosition + diceRollValue));
+        }
+
+        private static int globalPosition(int playerIndex, int currentIndividualPosition)
+        {
+            return ((playerIndex * startShift) + currentIndividualPosition) % sharedCellsCount;
         }
 
         private void lbl_sqr1_Click(object sender, EventArgs e)
--- LudoWinForm/LudoWinForm/Lableaxtensions.cs
+++ LudoWinForm/LudoWinForm/Lableaxtensions.cs
@@ -8,6 +8,15 @@ namespace LudoWinForm
 {
     public static class Lableaxtensions
     {
+        public static Control MoveTo(this Control source, Control target)
+        {
+            if (target == null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
 
+            source.Location = target.Location;
+            return source;
+        }
     }
 }
